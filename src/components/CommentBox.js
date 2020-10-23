fb3da7c0f661cd9f3ef4cc3c603c109fcf2fcd03
CjeanHenn <carolynhennessey95@gmail.com>
2020-10-23 20:16:13 +0200

    finish hoc unit

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from 'actions'
+import requireAuth from 'components/requireAuth'
 
 class CommentBox extends Component {
   state = { comment: '' }
-
-  componentDidMount() {
-    this.shouldNavigateAway()
-  }
-
-  componentDidUpdate() {
-    this.shouldNavigateAway()
-  }
-
-  shouldNavigateAway() {
-    if (!this.props.auth) {
-      console.log('I need to leave!')
-    }
-  }
 
   handleChange = (event) => {
     this.setState({ comment: event.target.value })
@@ -46,8 +33,4 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return { auth: state.auth }
-}
-
-export default connect(mapStateToProps, actions)(CommentBox)
+export default connect(null, actions)(requireAuth(CommentBox))
